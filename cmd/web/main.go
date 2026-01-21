4e21f0174fb06e57de0f763a98e70fac8b004b44
Gerald Murphy <geramurp52@gmail.com>
2026-01-21 16:43:35 -0700

    Create the Reading List Model

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,15 +4,21 @@ import (
 	"flag"
 	"log"
 	"net/http"
+
+	"readinglist.murphy.io/internal/models"
 )
 
 type application struct {
+	readinglist *models.ReadinglistModel
 }
 
 func main() {
 	addr := flag.String("addr", ":80", "Http network address")
+	endpoint := flag.String("endpoint", "http://localhost:4000/v1/books", "Endpoint for the readinglist web service")
 
-	app := &application{}
+	app := &application{
+		readinglist: &models.ReadinglistModel{Endpoint: *endpoint},
+	}
 
 	srv := &http.Server{
 		Addr:    *addr,
